c9500f065593a0d242116313fb533f7d2559e3af
FlameLine <taus.anarchy@gmail.com>
2021-10-17 17:28:47 +0300

    Added delScrap function

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from scrapping import Scraper
 import __init__
 
-# scrap = Scraper()
-# scrap.addNewScrap('a', 'z', url='https://zno.osvita.ua/')
 # TODO: Add check for help
-scrap = Scraper()
-scrap.addNewScrap('a', 'зно', url='https://zno.osvita.ua/')
